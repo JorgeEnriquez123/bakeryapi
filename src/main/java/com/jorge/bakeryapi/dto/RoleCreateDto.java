7311e2000aaa5b7a9fd273760304e9b5446bc615
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2023-12-10 23:56:52 -0500

    User and Role DTO Added

---
+++ src/main/java/com/jorge/bakeryapi/dto/RoleCreateDto.java
@@ -0,0 +1,10 @@
+package com.jorge.bakeryapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoleCreateDto {
+    private String name;
+}
---
+++ src/main/java/com/jorge/bakeryapi/dto/UserCreateDto.java
@@ -0,0 +1,10 @@
+package com.jorge.bakeryapi.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class UserCreateDto {
+    private String username;
+    private String password;
+}
