4edce15ecac1728a685ed248a6cfcea7f3e73bbe
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2023-12-10 23:48:59 -0500

    User and Role Repository Added

---
+++ src/main/java/com/jorge/bakeryapi/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.jorge.bakeryapi.repository;
+
+import com.jorge.bakeryapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/jorge/bakeryapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jorge.bakeryapi.repository;
+
+import com.jorge.bakeryapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
